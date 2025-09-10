2b1501ce99c14df881b48b158e85d644efef5c87
Vivekkushwahaofficial <vivek39101@gmail.com>
2025-09-10 18:57:37 +0530

    Updated code

--- src/If_Else/FourDigitNumber.java
+++ src/If_Else/FourDigitNumber.java
@@ -14,5 +14,16 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+public class FourDigitNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        if (n % 5 == 0 || n % 3 == 0)
+            System.out.print("Divisible by 5 or 3: ");
+        else
+            System.out.print("Not divisible by 3 or 5: ");
+    }
+}
 
 
