8f98bc99e6c9f2ffde4eede9f9f6dfd0d4af7483
Vivekkushwahaofficial <vivek39101@gmail.com>
2025-09-15 23:33:20 +0530

    Updated code

--- src/Pattern_Printing/AlphabetSquare.java
+++ src/Pattern_Printing/AlphabetSquare.java
@@ -1,12 +1,28 @@
 package Pattern_Printing;
 import java.util.*;
+//public class AlphabetSquare {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i=0; i<=n; i++) {
+//            for(int j=0; j<=n; j++){
+//                System.out.print((char)(i+65));
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 public class AlphabetSquare {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=n; j++){
-                System.out.print((char)(i+65));
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++){
+                if(i % 2 == 0) {
+                    System.out.print((char) (i + 65) + " ");
+                } else {
+                    System.out.print((char) (i + 97) + " ");
+                }
             }
             System.out.println();
         }
